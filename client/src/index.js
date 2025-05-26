ec96f959f5220a9f5d16aed7c2bf1c478b922c5d
Enver Bilal Birinci <enver.birinci@inavitas.com>
2025-05-26 23:37:24 +0300

    React Router

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/app";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import {
-  ApolloProvider,
-} from "@apollo/client";
-import client from './apollo'
-
+import { ApolloProvider } from "@apollo/client";
+import client from "./apollo";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ApolloProvider client={client}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </ApolloProvider>
 );
