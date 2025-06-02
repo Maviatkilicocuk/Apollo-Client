1d18be125d5dbd7e8059771725626be818a9185b
Enver Bilal Birinci <enver.birinci@inavitas.com>
2025-06-02 03:27:44 +0300

    Gece 4 oldu yat

--- Odev_4/client/src/index.js
+++ Odev_4/client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App';
 
 import { ApolloProvider} from '@apollo/client';
 import client from './apollo';
